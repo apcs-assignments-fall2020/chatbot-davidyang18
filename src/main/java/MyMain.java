566bd54f322e5cd80352ee9484f96a2e5cdd7b99
davidyang18 <2817822828@qq.com>
2020-09-01 17:31:30 +0000

    9/1/20 HW ChatBot

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,6 +4,12 @@ public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.print("Hellloooo! What your name?");
+        String name = scan.next();
+        System.out.println("Glad to know you," + name);
+        System.out.print("What's your favorite fruit?");
+        String fruit = scan.next();
+        System.out.println("I love " + fruit + " too!");
 
         scan.close();
     }
